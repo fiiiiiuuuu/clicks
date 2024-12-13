21927a58b3781652de8d77b11b395487521fdf25
Niebieski <157232546+fiiiiiuuuu@users.noreply.github.com>
2024-12-13 16:23:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,27 +19,26 @@ def count_clicks(api_key, key):
     return stats['response']['stats'][0]['views']
 
 
-def is_shorten_link(parsed_user_url):
-    return parsed_user_url.netloc == "vk.cc"
-
+def is_shorten_link(user_url, api_key):
+    vk_url = 'https://api.vk.com/method/utils.getShortLink'
+    response = requests.post(vk_url, data={"access_token": api_key, "url": user_url, "v": "5.199"})
+    response.raise_for_status()
+    if 'error' in response.json():
+        return user_url
+    else:
+        return shorten_link(user_url, api_key)
+        
 
 def main():
     load_dotenv('.env')
-    VK_API_KEY = os.environ['VK_API_KEY']
+    vk_api_key = os.environ['VK_API_KEY']
     user_url = input("Введите ссылку:")
-
-    if not user_url.startswith('https://' or 'http://'):
-        user_url = 'http://' + user_url
     
-    parsed_user_url = urllib.parse.urlsplit(user_url)
-    if is_shorten_link(parsed_user_url):
-        short_link = user_url
-    else:
-        short_link = shorten_link(user_url, VK_API_KEY)
+    short_link = is_shorten_link(user_url, vk_api_key)
     parsed_url = urllib.parse.urlparse(short_link)
     try:
         print(f"Сокращенная ссылка: {short_link}")
-        print(f"Статистика переходов: {count_clicks(VK_API_KEY, parsed_url)}")
+        print(f"Статистика переходов: {count_clicks(vk_api_key, parsed_url)}")
     except KeyError:
         print("Ответ VK не соответствует параметрам, проверьте ссылку!")
     except requests.exceptions.HTTPError:
